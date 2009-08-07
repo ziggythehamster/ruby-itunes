8456a85618b531976354637547d9eecbc4034109
ziggythehamster <ziggy@ignition-project.com>
2009-08-06 23:41:51 -0700

    Change the class name to be ITunes::Application rather than ITunesApp

--- lib/itunes_app.rb
+++ lib/itunes_app.rb
@@ -1,116 +1,119 @@
 require "win32ole"
 
-# As far as iTunes is concerned, there is only one
-# iTunesApp, but we allow creating more than one.
-# You never know, maybe it'll matter.
-class ITunesApp
-
-	# Called when you call ITunesApp.new
-	def initialize
-		@app = WIN32OLE.new("iTunes.Application")
-		raise "iTunes failed to initialize" unless @app # FIXME: Throw an exception object
-	end
-
-	######## PLAYER CONTROLS ##################################################
+module ITunes
 	
-	# Reposition to the beginning of the current track or go to the previous
-	# track if already at start of current track.
-	def back_track
-		return @app.BackTrack()
-	end
+	# iTunes doesn't permit itself to run more than once,
+	# but that doesn't stop us from allowing multiple instances
+	# of ITunes::Application
+	class Application
 
-	# Skip forward in a playing track.
-	def fast_forward
-		return @app.FastForward()
-	end
+		# Called when you call ITunes::Application.new
+		def initialize
+			@app = WIN32OLE.new("iTunes.Application")
+			raise "iTunes failed to initialize" unless @app # FIXME: Throw an exception object
+		end
 
-	# Advance to the next track in the current playlist.
-	def next_track
-		return @app.NextTrack()
-	end
+		######## PLAYER CONTROLS ##################################################
+		
+		# Reposition to the beginning of the current track or go to the previous
+		# track if already at start of current track.
+		def back_track
+			return @app.BackTrack()
+		end
 
-	# Pause playback.
-	def pause
-		return @app.Pause()
-	end
+		# Skip forward in a playing track.
+		def fast_forward
+			return @app.FastForward()
+		end
 
-	# Play the currently targeted track.
-	def play
-		return @app.Play()
-	end
+		# Advance to the next track in the current playlist.
+		def next_track
+			return @app.NextTrack()
+		end
 
-	# Play the specified file path, adding it to the library if not already present.
-	def play_file(file_name = "")
-		return @app.PlayFile(file_name)
-	end
+		# Pause playback.
+		def pause
+			return @app.Pause()
+		end
 
-	# Toggle the playing/paused state of the current track.
-	def play_pause
-		return @app.PlayPause()
-	end
+		# Play the currently targeted track.
+		def play
+			return @app.Play()
+		end
 
-	# Return to the previous track in the current playlist.
-	def previous_track
-		return @app.PreviousTrack()
-	end
+		# Play the specified file path, adding it to the library if not already present.
+		def play_file(file_name = "")
+			return @app.PlayFile(file_name)
+		end
 
-	# Disable fast forward/rewind and resume playback, if playing.
-	def resume
-		return @app.Resume()
-	end
+		# Toggle the playing/paused state of the current track.
+		def play_pause
+			return @app.PlayPause()
+		end
 
-	# Skip backwards in a playing track.
-	def rewind
-		return @app.Rewind()
-	end
+		# Return to the previous track in the current playlist.
+		def previous_track
+			return @app.PreviousTrack()
+		end
 
-	# Stop playback.
-	def stop
-		return @app.Stop()
-	end
+		# Disable fast forward/rewind and resume playback, if playing.
+		def resume
+			return @app.Resume()
+		end
 
-	######## MISCELLANEOUS METHODS ############################################
+		# Skip backwards in a playing track.
+		def rewind
+			return @app.Rewind()
+		end
 
-	# Open the specified iTunes Store or streaming audio URL.
-	def open_url(url)
-		return @app.OpenURL(url)
-	end
+		# Stop playback.
+		def stop
+			return @app.Stop()
+		end
 
-	# Exits the iTunes application.
-	def quit
-		@app.Quit
-	end
+		######## MISCELLANEOUS METHODS ############################################
 
-	######## PROPERTIES #######################################################
-	
+		# Open the specified iTunes Store or streaming audio URL.
+		def open_url(url)
+			return @app.OpenURL(url)
+		end
 
-	# The full path to the current iTunes library XML file.
-	def library_xml_path
-		return @app.LibraryXMLPath()
-	end
+		# Exits the iTunes application.
+		def quit
+			@app.Quit
+		end
 
-	# True if sound output is muted.
-	def muted?
-		return @app.Mute
-	end
+		######## PROPERTIES #######################################################
+		
 
-	# Sets if the sound output is muted.
-	def muted=(muted)
-		@app.Mute = muted
-	end
+		# The full path to the current iTunes library XML file.
+		def library_xml_path
+			return @app.LibraryXMLPath()
+		end
 
-	# Returns the version of the iTunes application
-	def version
-		return @app.Version()
-	end
+		# True if sound output is muted.
+		def muted?
+			return @app.Mute
+		end
 
-	# Returns the current sound output volume (range 0..100)
-	def volume
-		return @app.SoundVolume()
-	end
+		# Sets if the sound output is muted.
+		def muted=(muted)
+			@app.Mute = muted
+		end
 
-	# Sets the sound output volume (range 0..100)
-	def volume=(volume)
-		@app.SoundVolume = volume
+		# Returns the version of the iTunes application
+		def version
+			return @app.Version()
+		end
+
+		# Returns the current sound output volume (range 0..100)
+		def volume
+			return @app.SoundVolume()
+		end
+
+		# Sets the sound output volume (range 0..100)
+		def volume=(volume)
+			@app.SoundVolume = volume
+		end
 	end
 end
